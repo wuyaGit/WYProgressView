d4d40c424fe674ae79e3483cc65e75aa615fdcbc
youngBi <407671883@qq.com>
2017-12-22 16:14:23 +0800

    delete sizeWithFont

--- WYProgressView/WYProgressView.m
+++ WYProgressView/WYProgressView.m
@@ -162,28 +162,10 @@
     CGFloat xCenter = self.frame.size.width * 0.5;
     CGFloat yCenter = self.frame.size.height * 0.5;
     
-    // 判断系统版本
-    if ([[UIDevice currentDevice].systemVersion floatValue] >= 7.0) {
-        CGSize strSize = [text sizeWithAttributes:attributes];
-        CGFloat strX = xCenter - strSize.width * 0.5;
-        CGFloat strY = yCenter - strSize.height * 0.5;
-        [text drawAtPoint:CGPointMake(strX, strY) withAttributes:attributes];
-    } else {
-        CGSize strSize;
-        NSAttributedString *attrStr = nil;
-        if (attributes[NSFontAttributeName]) {
-            strSize = [text sizeWithFont:attributes[NSFontAttributeName]];
-            attrStr = [[NSAttributedString alloc] initWithString:text attributes:attributes];
-        } else {
-            strSize = [text sizeWithFont:[UIFont systemFontOfSize:[UIFont systemFontSize]]];
-            attrStr = [[NSAttributedString alloc] initWithString:text attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:[UIFont systemFontSize]]}];
-        }
-        
-        CGFloat strX = xCenter - strSize.width * 0.5;
-        CGFloat strY = yCenter - strSize.height * 0.5;
-        
-        [attrStr drawAtPoint:CGPointMake(strX, strY)];
-    }
+    CGSize strSize = [text sizeWithAttributes:attributes];
+    CGFloat strX = xCenter - strSize.width * 0.5;
+    CGFloat strY = yCenter - strSize.height * 0.5;
+    [text drawAtPoint:CGPointMake(strX, strY) withAttributes:attributes];
 }
 
 #pragma mark - setter
